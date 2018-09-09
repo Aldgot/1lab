84a3c65ecc20479fdb4ab3ca190df0f1a30b9c67
Влад <Sparkjet385@gmail.com>
2018-09-09 22:22:46 +0300

    1lab

---
+++ src/com/example/FirstLab/FirstLab.java
@@ -0,0 +1,8 @@
+package com.example.FirstLab;
+
+public class FirstLab {
+
+    public static void main (String[] arqs) {
+        System.out.println("Имя: Мараренко Владислав Александрович\nФакультет: ИКС \nГруппа: АТ-161");
+    }
+}
